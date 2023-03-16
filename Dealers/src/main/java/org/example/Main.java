65f58fdb5db982370fb052807cbdde956511f83f
Djabeu <rafal.chojnowski1988@gmail.com>
2023-03-16 20:41:48 +0100

    find li elements

--- Dealers/src/main/java/org/example/Main.java
+++ Dealers/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        WebClient client = new WebClient();
+        client.getOptions().setJavaScriptEnabled(false);
+        client.getOptions().setCssEnabled(false);
+        HtmlPage searchPage = client.getPage("https://79element.pl/srebrne-monety-1-oz/?orderby=price&InStockbutton=sort&product-page=1");
+
+        for(DomElement domElement : searchPage.getElementsByTagName("ul")) {
+            System.out.println(domElement);
+
+            System.out.println(domElement.getAttribute("class"));
+
+            if(domElement.getAttribute("class").contains("products")) {
+                for(DomElement child : domElement.getChildElements()) {
+                    System.out.println(child.getTagName());
+                }
+            }
+
+            System.out.println();
+        }
+
     }
 }
